9e48a3513c0b8d5478a1c5a79a54507184b6b014
junior <jpegrande@gmail.com>
2020-06-25 20:11:28 -0400

    bash

--- tribeam.sh
+++ tribeam.sh
@@ -78,26 +78,26 @@ cleanup(){
 # Prgram functions
 downloadAndParseCatalog(){
 
-	# Download catalog file from apple server
-	#-------------------------------
-	echo -e "Downloading macOS catalog from swscan.apple.com..."
-	if ! curl --fail -s -f -o "$tmpDir/catalog.gz" "$1"; then error "Failed to download catalog" && exit; fi
-	gunzip -k "$tmpDir/catalog.gz"
-	rm "$tmpDir/catalog.gz"
+# Download catalog file from apple server
+#-------------------------------
+echo -e "Downloading macOS catalog from swscan.apple.com..."
+if ! curl --fail -s -f -o "$tmpDir/catalog.gz" "$1"; then error "Failed to download catalog" && exit; fi
+gunzip -k "$tmpDir/catalog.gz"
+rm "$tmpDir/catalog.gz"
 
 
-	# Parse catalog file into arrays
-	#-------------------------------
-	versionsArray=($(getListOfVersions))
+# Parse catalog file into arrays
+#-------------------------------
+versionsArray=($(getListOfVersions))
 
-	appleDiagnosticsArray=($(findLinkInCatalog AppleDiagnostics.dmg "$tmpDir/catalog"))
-	appleDiagnosticsChunklistArray=($(findLinkInCatalog AppleDiagnostics.chunklist "$tmpDir/catalog"))
-	baseSystemArray=($(findLinkInCatalog BaseSystem.dmg "$tmpDir/catalog"))
-	baseSystemChunklistArray=($(findLinkInCatalog BaseSystem.chunklist "$tmpDir/catalog"))
-	installInfoArray=($(findLinkInCatalog InstallInfo.plist "$tmpDir/catalog"))
-	installESDArray=($(findLinkInCatalog InstallESDDmg.pkg "$tmpDir/catalog"))
+appleDiagnosticsArray=($(findLinkInCatalog AppleDiagnostics.dmg "$tmpDir/catalog"))
+appleDiagnosticsChunklistArray=($(findLinkInCatalog AppleDiagnostics.chunklist "$tmpDir/catalog"))
+baseSystemArray=($(findLinkInCatalog BaseSystem.dmg "$tmpDir/catalog"))
+baseSystemChunklistArray=($(findLinkInCatalog BaseSystem.chunklist "$tmpDir/catalog"))
+installInfoArray=($(findLinkInCatalog InstallInfo.plist "$tmpDir/catalog"))
+installESDArray=($(findLinkInCatalog InstallESDDmg.pkg "$tmpDir/catalog"))
 
-	rm "$tmpDir/catalog"
+rm "$tmpDir/catalog"
 
 }
 
@@ -135,46 +135,46 @@ downloadOS(){
 	banner
 	LATEST_VERSION=$(checkOSAvaibility "10.16")
 
-	# User input for selecting release type
-	echo -e "Which release you want?"
-	select RELEASETYPE in "Developer Release" "Beta Release" "Public Release"; do
-		case $RELEASETYPE in
-			Developer* ) CATALOGTYPE="-${LATEST_VERSION}seed"; break;;
-			Beta* ) CATALOGTYPE="-${LATEST_VERSION}beta"; break;;
-			Public* ) break;;
-		esac
-	done
+# User input for selecting release type
+echo -e "Which release you want?"
+select RELEASETYPE in "Developer Release" "Beta Release" "Public Release"; do
+	case $RELEASETYPE in
+		Developer* ) CATALOGTYPE="-${LATEST_VERSION}seed"; break;;
+		Beta* ) CATALOGTYPE="-${LATEST_VERSION}beta"; break;;
+		Public* ) break;;
+	esac
+done
 
 
-	downloadAndParseCatalog "https://swscan.apple.com/content/catalogs/others/index${CATALOGTYPE}-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
+downloadAndParseCatalog "https://swscan.apple.com/content/catalogs/others/index${CATALOGTYPE}-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
 
-                            
-	banner
-	# User input for selecting macOS versions
-	echo -e  "Select macOS version : "
-	select MACVERSION in "${versionsArray[@]}"; do
-		if [[ $REPLY -le ${#versionsArray[@]} && $REPLY -gt 0 ]]
-		then
 
-			# Dont break sequence (It's sequenced with $fileNames[@])
-			links=(${baseSystemArray[$[$REPLY - 1]]})
-			fileNames=("BaseSystem.dmg")
+banner
+# User input for selecting macOS versions
+echo -e  "Select macOS version : "
+select MACVERSION in "${versionsArray[@]}"; do
+	if [[ $REPLY -le ${#versionsArray[@]} && $REPLY -gt 0 ]]
+	then
 
-			# Ask user to download macOS or only print links
-			while true; do read -p "would you like to proceed to download? [y/n] " yn
+		# Dont break sequence (It's sequenced with $fileNames[@])
+		links=(${baseSystemArray[$[$REPLY - 1]]})
+		fileNames=("BaseSystem.dmg")
+
+		# Ask user to download macOS or only print links
+		while true; do read -p "would you like to proceed to download? [y/n] " yn
 			echo -e ""
 			if [[ $yn == y ]]; then
 
-				# make source directory
-				if [ ! -d "$BaseDir" ]; then mkdir "$BaseDir"; fi
+			# make source directory
+			if [ ! -d "$BaseDir" ]; then mkdir "$BaseDir"; fi
 
-				# Download files into $BaseDir from $links[@]
-				for i in {0..0}; do
-					curl -f -o "$BaseDir/${fileNames[$i]}" "${links[$i]}"
-				done && break;
+			# Download files into $BaseDir from $links[@]
+			for i in {0..0}; do
+				curl -f -o "$BaseDir/${fileNames[$i]}" "${links[$i]}"
+			done && break;
 
-			elif [[ $yn == n ]]; then
-				for link in "${links[@]}"; do print $link; done && break; #&& exit;
+		elif [[ $yn == n ]]; then
+			for link in "${links[@]}"; do print $link; done && break; #&& exit;
 			fi
 		done
 
@@ -188,7 +188,7 @@ banner
 
 extract(){
 	if
-	7z e -tdmg $BaseDir BaseSystem.dmg -bsp0 -bso0 -o/$BaseDir *.hfs
+		7z e -tdmg $BaseDir BaseSystem.dmg -bsp0 -bso0 -o/$BaseDir *.hfs
 	then
 		mv $BaseDir/"4.hfs" $BaseDir/"base.hfs"
 	else
@@ -201,26 +201,26 @@ selectusb(){
 
 	readarray -t lines < <(lsblk --nodeps -no name,size | grep "sd")
 
-	# Prompt the user to select one of the lines.
-	echo -e "${RED}WARNING!!! SELECTING THE WRONG DISK MAY WIPE YOUR PC AND ALL DATA!!!${NOCOLOR}"
-	echo -e "Please select the usb-drive!!"
-	select choice in "${lines[@]}"; do
-		[[ -n $choice ]] || { echo -e ">>> Invalid Selection !" >&2; continue; }
-		break # valid choice was made; exit prompt.
-	done
+# Prompt the user to select one of the lines.
+echo -e "${RED}WARNING!!! SELECTING THE WRONG DISK MAY WIPE YOUR PC AND ALL DATA!!!${NOCOLOR}"
+echo -e "Please select the usb-drive!!"
+select choice in "${lines[@]}"; do
+	[[ -n $choice ]] || { echo -e ">>> Invalid Selection !" >&2; continue; }
+	break # valid choice was made; exit prompt.
+done
 
-	# Split the chosen line into ID and serial number.
-	read -r id sn unused <<<"$choice"
+# Split the chosen line into ID and serial number.
+read -r id sn unused <<<"$choice"
 }
 
 partformat(){
 
 	if
-	umount $(echo /dev/$id?*) || :
-	sleep 3s
-	sgdisk --zap-all /dev/$id
-	sgdisk /dev/$id --new=0:0:+300MiB -t 0:ef00
-	partprobe $(echo /dev/$id?*)
+		umount $(echo /dev/$id?*) || :
+		sleep 3s
+		sgdisk --zap-all /dev/$id
+		sgdisk /dev/$id --new=0:0:+300MiB -t 0:ef00
+		partprobe $(echo /dev/$id?*)
 	then
 		sgdisk -e /dev/$id --new=0:0: -t 0:af00
 		partprobe $(echo /dev/$id?*)
@@ -234,55 +234,55 @@ dding(){
 	banner
 	echo -e "Copying macOS img to drive $id!"
 	if
-	dd bs=8M if="$BaseDir/base.hfs" of=$(echo /dev/$id)2 status=progress oflag=sync
+		dd bs=8M if="$BaseDir/base.hfs" of=$(echo /dev/$id)2 status=progress oflag=sync
 	then
 		umount $(echo /dev/$id?*) || :
 		sleep 3s
 	else
 		exit 1
-	fi
-}
+		fi
+	}
 
 installoc(){
 	banner
 	# Format the EFI partition for opencore
 	# and mount it in the /mnt.
 	if
-	mkfs.fat -F32 -n EFI $(echo /dev/$id)1
+		mkfs.fat -F32 -n EFI $(echo /dev/$id)1
 	then
 		mount -t vfat  $(echo /dev/$id)1 /mnt/ -o rw,umask=000; sleep 3s
 	else
 		exit 1
 	fi
 
-	# Install opencore.
-	echo -e "Installing OpenCore!!"
-	sleep 3s
+# Install opencore.
+echo -e "Installing OpenCore!!"
+sleep 3s
 
-	# OpenCore Downloader fuction.
-	banner
-	if
+# OpenCore Downloader fuction.
+banner
+if
 	cd ${BaseDir}
 	curl "https://api.github.com/repos/acidanthera/OpenCorePkg/releases/latest" \
-	| grep -i browser_download_url \
-	| grep RELEASE.zip \
-	| cut -d'"' -f4 \
-	| wget -qi -
-	then
-		banner
-		sleep 3s
-		7z x *RELEASE.zip -bsp0 -bso0 -o/mnt/
-	else
-		exit 1
-	fi
-	sleep 3s
-	chmod +x /mnt/
-	umount $(echo /dev/$id)1
-	mount -t vfat  $(echo /dev/$id)1 /mnt/ -o rw,umask=000
-	cd ..
-	sleep 3s
+		| grep -i browser_download_url \
+		| grep RELEASE.zip \
+		| cut -d'"' -f4 \
+		| wget -qi -
+		then
+			banner
+			sleep 3s
+			7z x *RELEASE.zip -bsp0 -bso0 -o/mnt/
+		else
+			exit 1
+fi
+sleep 3s
+chmod +x /mnt/
+umount $(echo /dev/$id)1
+mount -t vfat  $(echo /dev/$id)1 /mnt/ -o rw,umask=000
+cd ..
+sleep 3s
 
-	echo -e "Installation finished, open /mnt/ and edit oc for your machine!!"
+echo -e "Installation finished, open /mnt/ and edit oc for your machine!!"
 }
 
 banner
